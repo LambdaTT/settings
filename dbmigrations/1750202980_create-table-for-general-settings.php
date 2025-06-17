a7e8f34db9b8bda11be9c6464110c2bd883157cd
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2025-06-17 20:50:44 -0300

    added migrations

---
+++ dbmigrations/1750202980_create-table-for-general-settings.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Settings\Migrations;
+
+use SplitPHP\DbMigrations\Migration;
+use SplitPHP\Database\DbVocab;
+
+class CreateTableForGeneralSettings extends Migration
+{
+  public function apply()
+  {
+    $this->Table('STT_SETTINGS')
+      ->id('id_stt_settings') // int primary key auto increment
+      ->datetime('dt_updated')->setDefaultValue(DbVocab::SQL_CURTIMESTAMP())
+      ->int('id_iam_user_updated')->nullable()->setDefaultValue(null)
+      ->string('ds_context', 60)
+      ->string('ds_format', 20)->setDefaultValue('text')
+      ->string('ds_fieldname', 60)
+      ->text('tx_fieldvalue')->nullable()->setDefaultValue(null)
+      ->Foreign('id_iam_user_updated')->references('id_iam_user')->atTable('IAM_USER')->onUpdate(DbVocab::FKACTION_CASCADE)->onDelete(DbVocab::FKACTION_SETNULL);
+  }
+}
