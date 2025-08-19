63bf8323242f898fce96faa6826c092b671e1fb3
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2025-08-19 12:01:10 -0300

    fix routes

--- routes.php
+++ routes.php
@@ -4,66 +4,63 @@ namespace Settings\Routes;
 
 use SplitPHP\Request;
 use SplitPHP\WebService;
+use SplitPHP\Exceptions\Unauthorized;
 
 class Settings extends WebService
 {
   public function init()
   {
+    $this->setAntiXsrfValidation(false);
     /////////////////
     // GENERAL SETTINGS ENDPOINTS:
     /////////////////
-
     $this->addEndpoint('GET', "/v1/from-context/?context?", function (Request $request) {
       $context = $request->getRoute()->params['context'];
 
-      $list = $this->getService('settings/director')->listByContext($context);
+      $object = $this->getService('settings/settings')->contextObject($context);
 
-      if (empty($list)) return $this->response->withStatus(404);
+      if (empty($object)) return $this->response->withStatus(404);
 
       return $this->response
         ->withStatus(200)
-        ->withData($list, false);
-    }, false);
+        ->withData($object, false);
+    });
 
     $this->addEndpoint('GET', "/v1/setting/?context?/?fieldname?", function (Request $request) {
       $context = $request->getRoute()->params['context'];
       $fieldname = $request->getRoute()->params['fieldname'];
 
-      $record = $this->getService('settings/director')->get($context, $fieldname);
+      $record = $this->getService('settings/settings')->get($context, $fieldname);
 
       if (empty($record)) return $this->response->withStatus(404);
 
       return $this->response
         ->withStatus(200)
         ->withData($record, false);
-    }, false);
+    });
 
     $this->addEndpoint('PUT', "/v1/setting", function (Request $request) {
-      // Auth user login:
-      if (!$this->getService('iam/session')->authenticate()) return $this->response->withStatus(401);
-
-      // Validate user permissions:
-      $this->getService('iam/permission')->validatePermissions([
+      $this->auth([
         'STT_SETTINGS' => 'U'
       ]);
 
-      $params = $request->getBody();
+      $body = $request->getBody();
 
-      foreach ($params as $field) {
+      foreach ($body as $field) {
         // Array de Objetos
         if (is_array($field)) {
-          $this->getService('settings/director')
+          $this->getService('settings/settings')
             ->change($field['ds_context'], $field['ds_fieldname'], $field['tx_fieldvalue'], $field['ds_format']);
         } else {
           // Objeto (Array associativo)
-          $this->getService('settings/director')
-            ->change($params['ds_context'], $params['ds_fieldname'], $params['tx_fieldvalue'], $field['ds_format']);
+          $this->getService('settings/settings')
+            ->change($body['ds_context'], $body['ds_fieldname'], $body['tx_fieldvalue'], $body['ds_format']);
           break;
         }
       }
 
       return $this->response->withStatus(204);
-    });
+    }, true);
 
     /////////////////
     // CUSTOM FIELDS ENDPOINTS:
@@ -74,30 +71,22 @@ class Settings extends WebService
       return $this->response
         ->withStatus(200)
         ->withData($this->getService('settings/customfield')->fieldsOfEntity($entityName));
-    }, false);
+    });
 
     $this->addEndpoint('POST', "/v1/custom-field", function (Request $request) {
-      // Auth user login:
-      if (!$this->getService('iam/session')->authenticate()) return $this->response->withStatus(401);
-
-      // Validate user permissions:
-      $this->getService('iam/permission')->validatePermissions([
-        'STT_SETTINGS' . '_CUSTOMFIELD' => 'C'
+      $this->auth([
+        'STT_SETTINGS_CUSTOMFIELD' => 'C'
       ]);
 
       $data = $request->getBody();
       return $this->response
         ->withStatus(201)
         ->withData($this->getService('settings/customfield')->createField($data));
-    });
+    }, true);
 
     $this->addEndpoint('DELETE', "/v1/custom-field/?entityName?/?fieldName?", function (Request $request) {
-      // Auth user login:
-      if (!$this->getService('iam/session')->authenticate()) return $this->response->withStatus(401);
-
-      // Validate user permissions:
-      $this->getService('iam/permission')->validatePermissions([
-        'STT_SETTINGS' . '_CUSTOMFIELD' => 'D'
+      $this->auth([
+        'STT_SETTINGS_CUSTOMFIELD' => 'D'
       ]);
 
       $entityName = $request->getRoute()->params['entityName'];
@@ -109,6 +98,19 @@ class Settings extends WebService
 
       return $this->response
         ->withStatus(204);
-    });
+    }, true);
+  }
+
+  private function auth(array $permissions)
+  {
+    if (!$this->getService('modcontrol/control')->moduleExists('iam')) return;
+
+    // Auth user login:
+    if (!$this->getService('iam/session')->authenticate())
+      throw new Unauthorized("Não autorizado.");
+
+    // Validate user permissions:
+    $this->getService('iam/permission')
+      ->validatePermissions($permissions);
   }
 }
